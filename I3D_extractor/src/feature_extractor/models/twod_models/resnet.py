6f5dcecae94e09a50b2c0f137ac4f28de41a23a8
turingks <turingks@outlook.com>
2026-05-10 17:02:25 +0000

    change ckpt path

--- I3D_extractor/src/feature_extractor/models/twod_models/resnet.py
+++ I3D_extractor/src/feature_extractor/models/twod_models/resnet.py
@@ -262,7 +262,8 @@ def resnet(depth, num_classes, without_t_stride, groups, temporal_module_name,
                    pooling_method=pooling_method)
 
     if imagenet_pretrained:
-        state_dict = model_zoo.load_url(model_urls['resnet{}'.format(depth)], map_location='cpu')
+        # state_dict = model_zoo.load_url(model_urls['resnet{}'.format(depth)], map_location='cpu')
+        state_dict = torch.load("ckpt/resnet50-19c8e357.pth")
         state_dict.pop('fc.weight', None)
         state_dict.pop('fc.bias', None)
         model.load_state_dict(state_dict, strict=False)
