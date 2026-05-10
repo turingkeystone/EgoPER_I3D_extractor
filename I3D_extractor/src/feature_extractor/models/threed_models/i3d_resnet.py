a6172ecee3e164ccd59c4e59dcc874357e5604d8
turingks <turingks@outlook.com>
2026-05-10 17:14:08 +0000

    change ckpt path

--- I3D_extractor/src/feature_extractor/models/threed_models/i3d_resnet.py
+++ I3D_extractor/src/feature_extractor/models/threed_models/i3d_resnet.py
@@ -339,8 +339,9 @@ def i3d_resnet(depth, num_classes, dropout, without_t_stride, **kwargs):
     model = I3D_ResNet(depth, num_classes=num_classes, dropout=dropout, without_t_stride=without_t_stride)
 
     new_model_state_dict = model.state_dict()
-    state_dict = model_zoo.load_url(model_urls['resnet{}'.format(depth)],
-                                    map_location='cpu', progress=True)
+    # state_dict = model_zoo.load_url(model_urls['resnet{}'.format(depth)],
+    #                                 map_location='cpu', progress=True)
+    state_dict = torch.load("ckpt/resnet50-19c8e357.pth", map_location='cpu')
     state_d = inflate_from_2d_model(state_dict, new_model_state_dict,
                                     skipped_keys=['fc'])
     model.load_state_dict(state_d, strict=False)
